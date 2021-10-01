60fdf02f560465be5eae5bdf4d2ab2711dcf4d75
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-10-01 18:57:51 +0300

    added and updated JWT related functions for API

--- src/auth/models/jwt.js
+++ src/auth/models/jwt.js
@@ -4,12 +4,13 @@
 const client = require('../../models/db');
 const { getToken } = require('../models/helpers');
 
+// This function is used for creating tokens for users:
 
 async function createToken(user_id) {
   try {
     const accessToken = getToken(user_id);
     const refreshToken = getToken(user_id, 'refresh');
-    let SQL = `INSERT INTO jwt (access_token,refresh_token,user_id) VALUES ($1,$2,$3) RETURNING *;`;
+    let SQL = `INSERT INTO JWT (access_token,refresh_token,user_id) VALUES ($1,$2,$3) RETURNING *;`;
     let tokenValues = [accessToken, refreshToken, user_id];
     let tokenQuery = await client.query(SQL, tokenValues);
     return tokenQuery.rows[0];
@@ -18,22 +19,11 @@ async function createToken(user_id) {
   }
 }
 
-// async function updateToken(user_id) {
-//   try {
-//     const accessToken = getToken(user_id);
-//     const refreshToken = getToken(user_id, 'refresh');
-//     let SQL = `UPDATE jwt SET access_token=$1,refresh_token=$2 WHERE user_id=$3;`;
-//     let tokenValues = [accessToken, refreshToken, user_id];
-//     let tokenQuery = await client.query(SQL, tokenValues);
-//     return { accessToken, refreshToken };
-//   } catch (e) {
-//     throw new Error(e);
-//   }
-// }
+// This function is used for deleting tokens for users:
 
 async function deleteToken(user_id) {
   try {
-    let SQL = `DELETE from jwt WHERE user_id=$1;`;
+    let SQL = `DELETE FROM JWT WHERE user_id=$1;`;
     let removeToken = [user_id];
     let tokenQuery = await client.query(SQL, removeToken);
     return tokenQuery;
@@ -42,9 +32,11 @@ async function deleteToken(user_id) {
   }
 }
 
+// This function is used to get user's tokens using user_id:
+
 async function getTokenByUserId(user_id) {
   try {
-    let SQL = `SELECT access_token,refresh_token from jwt WHERE user_id=$1;`;
+    let SQL = `SELECT access_token,refresh_token from JWT WHERE user_id=$1;`;
     let getToken = [user_id];
     let tokenQuery = await client.query(SQL, getToken);
     return tokenQuery.rows[0];
@@ -53,10 +45,12 @@ async function getTokenByUserId(user_id) {
   }
 }
 
+// This function is used to get all tokens depending on token type:
+
 async function getTokenRecord(token, type='access') {
   try {
-    let SQL = `SELECT * from jwt WHERE access_token=$1;`;
-    if(type === 'refresh') SQL = `SELECT * from jwt WHERE refresh_token=$1;`;
+    let SQL = `SELECT * from JWT WHERE access_token=$1;`;
+    if(type === 'refresh') SQL = `SELECT * from JWT WHERE refresh_token=$1;`;
 
     let getToken = [token];
     let tokenQuery = await client.query(SQL, getToken);
